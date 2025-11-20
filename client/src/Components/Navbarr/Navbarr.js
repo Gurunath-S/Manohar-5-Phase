619d8a171d811a352a44bd0d35a161eb2b0a3846
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-11-20 16:15:58 +0530

    Refactor Navbarr component with user access controls

--- client/src/Components/Navbarr/Navbarr.js
+++ client/src/Components/Navbarr/Navbarr.js
@@ -1,35 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import '../Navbarr/Navbarr.css';
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import images from '../../Components/Logo/mano.jpg'
+import images from '../../Components/Logo/mano.jpg';
+import { toast } from "react-toastify";
+import {  logout  } from '../../services/authService'
+import {REACT_APP_BACKEND_SERVER_URL} from '../../config/index'
 
 const Navbarr = () => {
-  return (
-  <> 
-  <div className='nav-bar'> 
-  <div className="positionn">
- 
-  
-    <Link to='/home'> 
-          <b style={{ cursor: 'pointer', color: 'white'  }}> Products </b>
-          </Link>
-          <Link to="/billing">
-            <b style={{ cursor: 'pointer', color: 'white' }}> Billing </b>
-          </Link>
-          <Link to="/restore">
-            <b style={{ cursor: 'pointer', color: 'white' }}> Restore </b>
-          </Link>
-          <Link to="/"> 
-          <h3 className='hom'> Home</h3> </Link>
-          <img src={images} alt='jewellery' className='imge'  />   
-        </div>
-        
-        </div>
-       
-  </>
-  )
-}
+  const [loading,setLoading]= useState(false)
+  const [users,setUsers] = useState("")
+  const location = useLocation();
 
-export default Navbarr
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  const username = localStorage.getItem("username") || "{}"
+  const access = user?.access || {};
+  const token = localStorage.getItem("token");
+
+  const canCreateUsers = access.userCreateAccess;
+  const canAccessGoldSmith = access.goldSmithAccess;
+  const canAccessItems = access.itemAccess;
+  const canAccessProduct = access.productAccess;
+  const canAccessRestore = access.restoreAccess;
+  const canAccessBilling = access.billingAccess;
+
+  const isMasterActive = [
+    "/admin",
+    "/additem",
+    "/addgoldsmith"
+  ].includes(location.pathname);
+
+  const handlelogout =()=>{
+    // const log = window.confirm("are you sure to logout?")
+    // if(log){
+      // logout
+    // }
+  }
+
+  return (
+    <div className="nav-bar">
+      {/* Logo */}
+      <img src={images} alt="Logo" className="imge" />
+
+      <div className="positionn">
+
+        {/* Products 
+        {canAccessProduct && (
+        <NavLink to="/home">Products</NavLink>
+         )} */}
+
+        {/* Billing */}
+        {canAccessBilling && (
+        <NavLink to="/billing">Billing</NavLink>
+        )}
+
+        {/* Restore */}
+        {canAccessRestore && (
+        <NavLink to="/restore">Restore</NavLink>
+        )}
+        {/* Master dropdown (show only if user has ANY master permissions) */}
+        {(canCreateUsers || canAccessItems || canAccessGoldSmith) && (
+          <div className="master-link">
+            <NavLink to="#" className={isMasterActive ? "active" : ""}>
+              Master
+            </NavLink>
+
+            <div className="master-dropdown">
+
+              {canCreateUsers && (
+                <NavLink to="/admin">Admin</NavLink>
+              )}
+
+              {canAccessItems && (
+                <NavLink to="/additem">Add Item</NavLink>
+              )}
+
+              {canAccessGoldSmith && (
+                <NavLink to="/addgoldsmith">Add Gold Smith</NavLink>
+              )}
+
+            </div>
+          </div>
+        )}
+
+        <NavLink to="/home" className="hom">
+          <FontAwesomeIcon icon={faHouse} />
+        </NavLink>
+      </div>
+      <NavLink to="/" onClick={handlelogout}>
+          <div className="userinfo">
+            <span className="userAvatar">{username?.charAt(0).toUpperCase()}</span>
+          </div>
+        </NavLink>
+    </div>
+  );
+};
+
+
+export default Navbarr;
