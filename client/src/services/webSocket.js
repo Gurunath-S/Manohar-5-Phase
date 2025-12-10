6067b559dddfef338944d69f7448147c6e8d6d2e
boobalan <boobalan-mca23>
2025-12-10 15:35:36 +0530

    add ws remove wss

--- client/src/services/webSocket.js
+++ client/src/services/webSocket.js
@@ -1,14 +1,15 @@
+
 let ws;
 let listeners = [];
 
 function initWebSocket() {
-  ws = new WebSocket("wss://94.136.190.133:6020/ws");
+  ws = new WebSocket("ws://94.136.190.133:6020/ws");
 
   ws.onopen = () => console.log("WS Connected");
 
   ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
-
+    console.log('weight data',data)
     // notify all listeners
     listeners.forEach((cb) => cb(data.weight));
   };
