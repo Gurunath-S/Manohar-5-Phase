348e392cc4869578067a21c4810ab696391990e8
boobalan <boobalan-mca23>
2025-12-10 15:38:51 +0530

    add https

--- client/src/services/webSocket.js
+++ client/src/services/webSocket.js
@@ -3,7 +3,7 @@ let ws;
 let listeners = [];
 
 function initWebSocket() {
-  ws = new WebSocket("ws://94.136.190.133:6020/ws");
+  ws = new WebSocket("https://94.136.190.133:6020");
 
   ws.onopen = () => console.log("WS Connected");
 
